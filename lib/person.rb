8a0e64696a9c6b7b92f29a30538dd3eed6192010
Chanson9892 <chan.hanson5@gmail.com>
2020-08-25 17:16:01 -0700

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,22 @@
+class Person
+
+  # name setter 
+  def name=(person_name)
+    @name = person_name
+  end
+  # name getter
+  def name
+    @name
+  end
+
+  # job setter 
+  def job=(person_job)
+    @job = person_job
+  end
+  # job getter
+  def job
+    @job
+  end
+
+
+end
